31e8f0421439d437847fa86561d0ad4883768ea9
Quiet0o <mikolajklosowski112@gmail.com>
2022-10-12 21:30:43 +0200

    adding mapbox to project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import mapboxgl from "mapbox-gl";
+import { useEffect, useRef, useState } from "react";
+
+mapboxgl.accessToken =
+  "pk.eyJ1IjoicXVpZXQtMG8iLCJhIjoiY2w5MzJrcDl1MG1zdjN2bW8zZjkxemtzNCJ9.BBLdw6P0tJFu1VLtm_0zDg";
 
 function App() {
+  const mapContainer = useRef(null);
+  const map = useRef(null);
+  const [lng, setLng] = useState(-70.9);
+  const [lat, setLat] = useState(42.35);
+  const [zoom, setZoom] = useState(9);
+
+  useEffect(() => {
+    if (map.current) return; // initialize map only once
+    map.current = new mapboxgl.Map({
+      container: mapContainer.current,
+      style: "mapbox://styles/mapbox/streets-v11",
+      center: [lng, lat],
+      zoom: zoom,
+    });
+  });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div ref={mapContainer} className="map-container" />
     </div>
   );
 }
