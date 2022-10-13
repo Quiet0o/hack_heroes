4cc92eaa41dc70b758e6deb1217f8e68b23e63b8
Quiet0o <mikolajklosowski112@gmail.com>
2022-10-14 01:25:59 +0200

    adding working popups

--- src/Map/Map.js
+++ src/Map/Map.js
@@ -4,8 +4,9 @@ import Map, {
   GeolocateControl,
   Marker,
   NavigationControl,
+  Popup,
 } from "react-map-gl";
-import "mapbox-gl/dist/mapbox-gl.css";
+// import "mapbox-gl/dist/mapbox-gl.css";
 import geoJson from "./sample_data.json";
 
 const MAPBOX_TOKEN = process.env.REACT_APP_MAPBOX_ACCESS_TOKEN;
@@ -16,26 +17,54 @@ function MapRender() {
     longitude: -122.4,
     zoom: 14,
   });
+  const [showPopup, setShowPopup] = useState({});
+  const [currentPlaceId, setCurrentPlaceId] = useState(null);
 
+  const handleMarkerClick = (id, lat, long) => {
+    setCurrentPlaceId(id);
+    setViewState({ ...viewState,latitude: lat, longitude: long });
+    console.log(viewState);
+  };
   return (
     <Map
       {...viewState}
       onMove={(evt) => setViewState(evt.viewState)}
       style={{ width: window.innerWidth, height: window.innerHeight }}
+
       mapStyle="mapbox://styles/mapbox/streets-v9"
       mapboxAccessToken={MAPBOX_TOKEN}
+      onViewStateChange={(viewState) => setViewState(viewState)}
     >
       <GeolocateControl position="top-right" />
       <FullscreenControl position="top-right" />
       <NavigationControl position="top-right" />
 
-      {geoJson.features.map((features) => (
+      {geoJson.features.map((features,index) => (
+        <>
         <Marker
+          key={index}
           longitude={features.geometry.coordinates[0]}
           latitude={features.geometry.coordinates[1]}
           color="blue"
+          cursor="pointer"
+          onClick={() => handleMarkerClick( features.id,features.geometry.coordinates[1], features.geometry.coordinates[0])}
         />
+        {features._id === currentPlaceId && (
+         <Popup
+         key={features.id}
+         longitude={features.geometry.coordinates[0]}
+         latitude={features.geometry.coordinates[1]}
+         closeButton={true}
+         closeOnClick={false}
+         onClose={() => setCurrentPlaceId(null)}
+         anchor="left"
+        >
+          <label> { features.properties.title}</label>
+        </Popup>
+        )}
+        </>
       ))}
+
     </Map>
   );
 }
