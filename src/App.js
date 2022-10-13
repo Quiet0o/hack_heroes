aa3dfb7d9294a7ae577fa8098749851592c2c966
Quiet0o <mikolajklosowski112@gmail.com>
2022-10-13 18:59:02 +0200

    adding sample data

--- src/App.js
+++ src/App.js
@@ -1,30 +1,10 @@
-import logo from "./logo.svg";
-import "./App.css";
-import mapboxgl from "mapbox-gl";
-import { useEffect, useRef, useState } from "react";
-
-mapboxgl.accessToken =
-  "pk.eyJ1IjoicXVpZXQtMG8iLCJhIjoiY2w5MzJrcDl1MG1zdjN2bW8zZjkxemtzNCJ9.BBLdw6P0tJFu1VLtm_0zDg";
+import Map from "./Map/Map";
 
 function App() {
-  const mapContainer = useRef(null);
-  const map = useRef(null);
-  const [lng, setLng] = useState(-70.9);
-  const [lat, setLat] = useState(42.35);
-  const [zoom, setZoom] = useState(9);
 
-  useEffect(() => {
-    if (map.current) return; // initialize map only once
-    map.current = new mapboxgl.Map({
-      container: mapContainer.current,
-      style: "mapbox://styles/mapbox/streets-v11",
-      center: [lng, lat],
-      zoom: zoom,
-    });
-  });
   return (
     <div>
-      <div ref={mapContainer} className="map-container" />
+      <Map></Map>
     </div>
   );
 }
